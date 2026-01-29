d8bb6ccbeaaef85b613de0977cfcd0cbeecc6563
Prince <p1r2i3n4c5e6@users.noreply.github.com>
2026-01-29 18:27:00 +0530

    Fix: Add DISPLAY env var for RPi GUI

--- rpi_deploy/run_rpi5.sh
+++ rpi_deploy/run_rpi5.sh
@@ -3,5 +3,9 @@
 # Activate the virtual environment
 source venv/bin/activate
 
+# Export display for GUI to show on the RPi screen
+export DISPLAY=:0
+export XAUTHORITY=/home/leader/.Xauthority
+
 # Run the Drone Control Software
 python3 main.py
